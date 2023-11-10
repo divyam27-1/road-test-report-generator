e437e9a2b161672082412d495d76f0e710a4a15e
divyam27-1 <divyam2701@gmail.com>
2023-11-11 00:38:42 +0530

    now it makes html for all non calculating parts of specific gravity, and all the build files and such

--- build-road_test_report_generator-Desktop_Qt_6_5_3_GCC_64bit-Debug/moc_mainwindow.cpp
+++ build-road_test_report_generator-Desktop_Qt_6_5_3_GCC_64bit-Debug/moc_mainwindow.cpp
@@ -51,15 +51,13 @@ static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers:
     "on_ind_data_scroll_valueChanged",
     "on_aiv_data_scroll_valueChanged",
     "on_pushButton_3_clicked",
-    "specificgravity",
-    "jsonDoc_1",
-    "QFile",
-    "file",
-    "on_spc_export_clicked"
+    "on_spc_export_clicked",
+    "on_save_fei_clicked",
+    "on_save_ss_clicked"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
-    uint offsetsAndSizes[36];
+    uint offsetsAndSizes[32];
     char stringdata0[11];
     char stringdata1[22];
     char stringdata2[1];
@@ -73,11 +71,9 @@ struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
     char stringdata10[32];
     char stringdata11[32];
     char stringdata12[24];
-    char stringdata13[16];
-    char stringdata14[10];
-    char stringdata15[6];
-    char stringdata16[5];
-    char stringdata17[22];
+    char stringdata13[22];
+    char stringdata14[20];
+    char stringdata15[19];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -96,11 +92,9 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_st
         QT_MOC_LITERAL(174, 31),  // "on_ind_data_scroll_valueChanged"
         QT_MOC_LITERAL(206, 31),  // "on_aiv_data_scroll_valueChanged"
         QT_MOC_LITERAL(238, 23),  // "on_pushButton_3_clicked"
-        QT_MOC_LITERAL(262, 15),  // "specificgravity"
-        QT_MOC_LITERAL(278, 9),  // "jsonDoc_1"
-        QT_MOC_LITERAL(288, 5),  // "QFile"
-        QT_MOC_LITERAL(294, 4),  // "file"
-        QT_MOC_LITERAL(299, 21)   // "on_spc_export_clicked"
+        QT_MOC_LITERAL(262, 21),  // "on_spc_export_clicked"
+        QT_MOC_LITERAL(284, 19),  // "on_save_fei_clicked"
+        QT_MOC_LITERAL(304, 18)   // "on_save_ss_clicked"
     },
     "MainWindow",
     "on_pushButton_clicked",
@@ -115,11 +109,9 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_st
     "on_ind_data_scroll_valueChanged",
     "on_aiv_data_scroll_valueChanged",
     "on_pushButton_3_clicked",
-    "specificgravity",
-    "jsonDoc_1",
-    "QFile",
-    "file",
-    "on_spc_export_clicked"
+    "on_spc_export_clicked",
+    "on_save_fei_clicked",
+    "on_save_ss_clicked"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -131,7 +123,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-      11,   14, // methods
+      12,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -139,17 +131,18 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   80,    2, 0x08,    1 /* Private */,
-       3,    1,   81,    2, 0x08,    2 /* Private */,
-       5,    0,   84,    2, 0x08,    4 /* Private */,
-       6,    0,   85,    2, 0x08,    5 /* Private */,
-       7,    1,   86,    2, 0x08,    6 /* Private */,
-       9,    0,   89,    2, 0x08,    8 /* Private */,
-      10,    1,   90,    2, 0x08,    9 /* Private */,
-      11,    1,   93,    2, 0x08,   11 /* Private */,
-      12,    0,   96,    2, 0x08,   13 /* Private */,
-      13,    2,   97,    2, 0x08,   14 /* Private */,
-      17,    0,  102,    2, 0x08,   17 /* Private */,
+       1,    0,   86,    2, 0x08,    1 /* Private */,
+       3,    1,   87,    2, 0x08,    2 /* Private */,
+       5,    0,   90,    2, 0x08,    4 /* Private */,
+       6,    0,   91,    2, 0x08,    5 /* Private */,
+       7,    1,   92,    2, 0x08,    6 /* Private */,
+       9,    0,   95,    2, 0x08,    8 /* Private */,
+      10,    1,   96,    2, 0x08,    9 /* Private */,
+      11,    1,   99,    2, 0x08,   11 /* Private */,
+      12,    0,  102,    2, 0x08,   13 /* Private */,
+      13,    0,  103,    2, 0x08,   14 /* Private */,
+      14,    0,  104,    2, 0x08,   15 /* Private */,
+      15,    0,  105,    2, 0x08,   16 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -161,7 +154,8 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
     QMetaType::Void, QMetaType::Int,    8,
     QMetaType::Void, QMetaType::Int,    8,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QJsonDocument, 0x80000000 | 15,   14,   16,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -198,11 +192,11 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<int, std::false_type>,
         // method 'on_pushButton_3_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'specificgravity'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<QJsonDocument, std::false_type>,
-        QtPrivate::TypeAndForceComplete<QFile, std::false_type>,
         // method 'on_spc_export_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_save_fei_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_save_ss_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
@@ -223,7 +217,9 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 6: _t->on_ind_data_scroll_valueChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
         case 7: _t->on_aiv_data_scroll_valueChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
         case 8: _t->on_pushButton_3_clicked(); break;
-        case 10: _t->on_spc_export_clicked(); break;
+        case 9: _t->on_spc_export_clicked(); break;
+        case 10: _t->on_save_fei_clicked(); break;
+        case 11: _t->on_save_ss_clicked(); break;
         default: ;
         }
     }
@@ -248,13 +244,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 11)
+        if (_id < 12)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 11;
+        _id -= 12;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 11)
+        if (_id < 12)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 11;
+        _id -= 12;
     }
     return _id;
 }
